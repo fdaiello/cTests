31bac700cbcf82ca28bd2d3cc0cba1c9976ea7fa
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-01-28 11:02:04 -0300

    Add project files.

---
+++ cTests/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cTests
+{
+    enum PointType
+    {
+        a,
+        b,
+        c
+    }
+    internal class Point
+    {
+        protected int x, y;
+        public override bool Equals(object obj)
+        {
+            //Check for null and compare run-time types.
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Point p = (Point)obj;
+                return (x == p.x) && (y == p.y);
+            }
+        }
+        internal Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override int GetHashCode()
+        {
+            return (this.x << 2) ^ this.y;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Point({0}, {1})", x, y);
+        }
+    }
+}
---
+++ cTests/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+
+int x = 0;
+
+for (; ; x++)
+{
+    Console.WriteLine(x);
+    x++;
+}
